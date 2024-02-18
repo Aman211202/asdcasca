c8f5cebb89085d783db15196318f850c6467c859
Aman211202 <86157258+Aman211202@users.noreply.github.com>
2024-02-18 15:55:54 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Журналистика</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Бізбен байланысу үшін</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart pulsate'>
+				<h2>
+					 Төменде көрсетілген деректер бойынша хабарландыру калдыруыңызды сураймын
+				</h2>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Жұматай Аружан Серікқызы</h2><br><br>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p style="font-size: 20px; font-weight: bold;">
+    <span style="font-size: 24px;">87771305157</span><br><br><br>
+    <span style="font-size: 24px;">Serikkyzyaruzhan88@mail.ru</span>
+</p>
+
+								<br>
+								<br>
+								<br>
+								<br>
+								<br><br><br>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://www.calltouch.ru/upload/medialibrary/c67/c6724bf63d384114c5fe37d7533ff376.png') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>"Журналистика - бұл тек күмәнданған профессия емес, адамгершілік, объективтік және жауаптылықты қаламдыруға сенімді еркінің атасы. Біз, журналисттер, қаһарман жанымызды, қоғамның адамдарын дамыту, оқыту және көңіл бұлағын дамыту үшін қолдайды. Жаңалықтың, дүниеге қолдау берудің да ұлы қабілетті салдары бар."</h2><br><br>
+							</div>
+							
+
+								<br>
+								<br>
+								<br>
+								<br>
+								<br><br><br>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://www.calltouch.ru/upload/medialibrary/c67/c6724bf63d384114c5fe37d7533ff376.png') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>САЙТТАР</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt' style="font-weight: bold;">
+    <p>
+         1. <a href="https://qazcorpus.kz/">https://qazcorpus.kz/</a> Қазақ тілінің Ұлттық тіл корпусы<br>
+         2. <a href="https://emle.kz/">https://emle.kz/</a> Қазақ тілінің орфографиялық электронды базасы<br>
+         3. <a href="https://tilqazyna.kz/">https://tilqazyna.kz/</a> Ш.Шаяхметов атындағы «Тіл-қазына» ұлттық ғылыми-практикалық орталығы<br>
+         4. <a href="https://termincom.kz/">https://termincom.kz/</a> Терминком сайты<br>
+         5. <a href="https://tbi.kz/">https://tbi.kz/</a> А.Байтұрсынұлы атындағы Тіл білімі институты<br>
+         6. <a href="https://sozdikqor.kz/">https://sozdikqor.kz/</a> Сөздік қор<br>
+         7. <a href="https://kazneb.kz/kk">https://kazneb.kz/kk</a> Қазақстандағы Ұлттық электронды кітапхана
+    </p><br><br><br><br>
+
+    <p style="font-size: 20px; font-weight: bold;">
+    <span style="font-size: 24px;">АЛ СІЗ ҚАНДАЙ САЙТТАРДЫ ҚОЛДАНАСЫЗ?</span><br><br><br>
+    
+</p>
+</div>
+
+						</div>
+					</div>
+				</div>
+				
+	</div>
+</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+
+<title>Журналистика</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD. The purpose of this project is to accurately emulate the PlayStation 3 in its entirety with the power of reverse engineering and community collaboration.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, home">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - The PlayStation 3 Emulator" />
+<meta property="og:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+<meta name="twitter:title" content="RPCS3 - The PlayStation 3 Emulator">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
+</head>
+<body>
+<img src="C:\OSPanel\domains\localhost\img\graphics\landing" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/home/inc-home-player.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'xUpw9bHmoac',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'gIcsUr7W00k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'60lq9pXzDic',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>Журналистке<br>
+							 Керек Құралдар</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 "Әлемнің жұрегіне, барынша жақындау үшін журналист болдым" 
+							</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(https://transkriptor.com/img/blogImages/en/transcription-software-for-journalists.webp) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="video-tx1-heading darkmode-txt">
+					<h2>Мәтін жазуға</h2>
+				</div>
+		</div>
+	</div>
+	<div class="landing-con-handheld darkmode-feature">
+		<div class="handheld-img-backdrop">
+		</div>
+		<div class="handheld-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				
+				<div class="video-con-divider">
+				</div>
+				<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Google docs </h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Google Docs - бұл Google компаниясының онлайн қызметі, мәтіндерді жасау мен өңдеу үшін қолданылады. Бұл сервис өзгертілмеген программа орнатылған жоқ, браузер арқылы жұмыс істеу мүмкіндігін береді. <br>
+									<br>
+									 Google Docs-тың негізгі функциялары және мүмкіндіктері:
+
+Документтерді жасау: Пайдаланушылар текст документтерін жасауға болады, немесе бастапқы үлгілерді қолдана отырып.
+
+Мәтінді өңдеу: Google Docs мәтінді өңдеу мүмкіндіктерін ұсынады, шрифттерді өзгерту, мәтін өлшемінің көлемінің, түсінің түрінің және т.б. жаңарту.
+
+Документты форматтау: Пайдаланушылар мәтінді форматтауға болады, тақырыптарды қосу, абзацдарды бұқару, тізімдер жасау және т.б.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url(https://bluescreen.kz/content/images/2023/10/main_image_post_6405.png) no-repeat center; background-size: cover; ">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url(https://images.velog.io/images/snoop2head/post/46cba18c-25fa-4321-9191-c425390f5a4a/WqDZWu-w_400x400.jpg) no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Bear notes </h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									Bear Notes - бұл мәтіндік есептіктерді жазу мен ұсыну үшін пайдаланылатын көмекші бағдарлама. Ол көптеген қолайлы функциялар мен интерфейс бар мәтіндік есептікті қолданушыларға қатысу үшін жаңа технологияларды қолдайды. <br>
+									<br>
+									 Bear Notes-тың негізгі функциялары:
+
+Жолдау және мәтіндік есептіктерді жасау: Bear Notes пайдаланушыларға мәтіндік есептіктерді жасауға және өңдеу үшін пайдалануы мүмкін.
+
+Мәтінді өңдеу: Көптеген көмекші функцияларды қолдана отырып, мәтінді өңдеу, мәтіндің шрифтін өзгерту және басқалар.
+
+Тізімдіктер және мәтіндің тізілімі: Bear Notes пайдаланушыларға мәтіндің тізімдерін өңдеуге және мәтіндің тізілімін өзгертуге мүмкіндік береді.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+							
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-con-discord darkmode-discord">
+	<div class="discord-img-overlay">
+		</div>
+		<div class="discord-left-logo">
+			<img alt="Discord Left Banner" src="/img/graphics/svg/discord-left.svg" style="width: 100%; height: 100%;">
+		</div>
+		<div class="discord-center-logo">
+			<img alt="Discord Logo" src="/img/graphics/svg/discord-center.svg" style="width: 100%; height: 100%;">
+		</div>
+		<div class="discord-right-logo">
+			<img alt="Discord Right Banner" src="/img/graphics/svg/discord-right.svg" style="width: 100%; height: 100%;">
+		</div>
+		
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h3>АУДИОНЫ МӘТІНГЕ АЙНАЛДЫРАТЫН ҚҰРАЛДАР</h3>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+  <p style="font-size: 25px; line-height: 1.6; ">
+    Writer.voice.tech<br>
+    Transcribe.kz<br>
+    Otranscrie.com<br>
+    Google docs<br>
+    KZSTT
+  </p>
+</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.2" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop">
+		</div>
+		<div class="github-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra">
+								<h2>ЖАСАНДЫ ИНТЕЛЛЕКТ</h2>
+								<div class="landing-con-divider" style="background: #9647c7">
+								</div>
+							</div>
+
+							<p style="font-size: 20px; font-weight: bold;">
+    <span style="font-size: 24px; background: #9647c7"">ЖИ журналистке көмекші ме, бәсекелес пе?</span><br><br><br>
+    
+</p>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 d-Id.com<br>
+									  KZTTS<br>
+									   WOMBO<br>
+										Dream<br>	
+										ChatGPT
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+		
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
